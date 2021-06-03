7886ec24c33ea84ce0d4e258299c332da7db649f
Pyrdacor <trobt@web.de>
2021-06-03 15:11:39 +0200

    Fixed nuget copy action

--- nuget.ps1
+++ nuget.ps1
@@ -3,5 +3,5 @@ $ErrorActionPreference = 'Stop';
 if ($isWindows) {
   Write-Host Copy nuget packages
   mkdir "nuget"
-  xcopy /Y /I "SonicArranger\bin\Release\*.nupkg" "nuget\*"
+  xcopy /Y /I "SonicArranger\bin\Release\netstandard2.0\*.nupkg" "nuget\*"
 }
